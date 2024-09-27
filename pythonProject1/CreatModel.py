335facf2a8401d6ccad648ac0a4323493b763061
StasFoster <111638493+StasFoster@users.noreply.github.com>
2024-09-27 11:41:39 +0400

    Добавлена логика подготовки датасета

--- pythonProject1/CreatModel.py
+++ pythonProject1/CreatModel.py
@@ -1,14 +1,22 @@
-from keras.models import Sequential
-from keras.layers import LSTM
+import tensorflow
+import keras
 import numpy as np
 
-x_train = np.random.rand(1000, 10) # Временное решение до создания корректного Dataset
-y_train = np.random.rand(1000,10) # Временное решение до создания корректного Dataset
+x_train = []
+y_train = []
+with open("Dataset/Train/vehicle.params") as f:
+    list_ = f.readlines()
+    for i in range(len(list_) - 30):
+        x_train.append(list_[i:i+30])
+with open("Dataset/Valid/vehicle.params") as f:
+    list_ = f.readlines()
+    for i in range(len(list_) - 30):
+        y_train.append(list_[i:i+30])
 
-model = Sequential()
-model.add(LSTM(units=64, activation='relu', input_dim=10))
-model.add(LSTM(units=64, activation='relu'))
-model.add(LSTM(units=1))
+model = keras.Sequential()
+model.add(keras.LSTM(units=64, activation='relu', input_dim=10))
+model.add(keras.LSTM(units=64, activation='relu'))
+model.add(keras.LSTM(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=10, batch_size=32)
 model.save('model_name.h5')
